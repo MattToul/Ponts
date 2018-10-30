19732b040c45e80b589692297b0c1b881c88fea3
Pokasuko <toulemont.matthieu.ovp@gmail.com>
2018-10-30 09:33:49 +0100

    Minor changes

--- Machine Learning/Perceptron/tp3-etu.py
+++ Machine Learning/Perceptron/tp3-etu.py
@@ -1,6 +1,6 @@
-import  numpy as np
+import numpy as np
 import matplotlib.pyplot as plt
-from tools import *
+from tools import gen_arti,make_grid,plot_data, plot_frontiere
 
 def Classifier(object):
     def __init__(self):
@@ -26,7 +26,7 @@ def get_usps(l,datax,datay):
         resx = datax[datay==l,:]
         resy = datay[datay==l]
         return resx,resy
-    tmp =   zip(*[get_usps(i,datax,datay) for i in l])
+    tmp =   list(zip(*[get_usps(i,datax,datay) for i in l]))
     tmpx,tmpy = np.vstack(tmp[0]),np.hstack(tmp[1])
     idx = np.random.permutation(range(len(tmpy)))
     return tmpx[idx,:],tmpy[idx]
@@ -37,15 +37,15 @@ def show_usps(data):
 
 ### Donnees artificielles
 plt.ion()
-xgentrain,ygentrain = gen_arti(data_type=0,sigma=0.5,nbex=1000,epsilon=0.1)
-xgentest,ygentest = gen_arti(data_type=0,sigma=0.5,nbex=1000,epsilon=0.1)
+xgentrain,ygentrain = gen_arti(data_type=0, sigma=0.5, nbex=1000, epsilon=0.1)
+xgentest,ygentest = gen_arti(data_type=0, sigma=0.5, nbex=1000, epsilon=0.1)
 plt.figure()
 plot_data(xgentrain,ygentrain)
 
 ### Donnees reelles
 plt.figure()
-xuspstrain,yuspstrain = load_usps("USPS_train.txt")
-xuspstest,yuspstest = load_usps("USPS_test.txt")
+xuspstrain,yuspstrain = load_usps("USPS/USPS_train.txt")
+xuspstest,yuspstest = load_usps("USPS/USPS_test.txt")
 x06train,y06train = get_usps([0,6],xuspstrain,yuspstrain)
 x06test,y06test = get_usps([0,6],xuspstest,yuspstest)
 show_usps(x06train[0])
@@ -53,8 +53,10 @@ show_usps(x06train[0])
 
 
 
+def f(X):
+    return np.linalg.norm(X, axis = 1)
 #### Pour la visualisation des couts
 #### En 2D, f etant une fonction de R^2 -> R
-grid,xx,yy = make_grid(xmin=-1,xmax=3,ymin=-1,ymax=3,step=50)
+grid, xx, yy = make_grid(xmin=-1, xmax=3, ymin=-1, ymax=3, step=50)
 plt.figure()
-plt.contourf(xx,yy,f(grid).reshape(xx.shape),256)
+plt.contourf(xx, yy, f(grid).reshape(xx.shape), 256)
